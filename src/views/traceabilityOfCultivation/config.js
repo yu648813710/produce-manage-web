4816b5bf905f6ae4733cf858040a2ad999362044
jiangkun523 <jiangkun13@camelotchina.com>
2019-11-08 15:08:35 +0800

    [姜坤]修改列表折行问题

--- src/views/traceabilityOfCultivation/config.js
+++ src/views/traceabilityOfCultivation/config.js
@@ -1,15 +1,15 @@
 const columns = [
-  { title: '序号', scopedSlots: { customRender: 'id' }, key: 'id' },
-  { title: '商品名称', scopedSlots: { customRender: 'productName' }, key: 'productName' },
-  { title: '产品品种', dataIndex: 'productBreed', key: 'productBreed' },
-  { title: '生产地', scopedSlots: { customRender: 'address' }, key: 'address' },
-  { title: '生产企业', scopedSlots: { customRender: 'productionCompany' }, key: 'productionCompany' },
-  { title: '生产日期', dataIndex: 'productionDate', key: 'productionDate' },
-  { title: '保质期(天)', dataIndex: 'expiryTime', key: 'expiryTime' },
-  { title: '木耳图片', scopedSlots: { customRender: 'productPicture' }, key: 'productPicture', align: 'center' },
-  { title: '溯源二维码', scopedSlots: { customRender: 'qrcodeId' }, key: 'qrcodeId', align: 'center' },
-  { title: '状态', scopedSlots: { customRender: 'status' }, key: 'status' },
-  { title: '操作', key: 'operation', scopedSlots: { customRender: 'operation' }, align: 'center' }
+  { title: '序号', scopedSlots: { customRender: 'id' }, key: 'id', width: 70 },
+  { title: '商品名称', scopedSlots: { customRender: 'productName' }, key: 'productName', width: 150 },
+  { title: '产品品种', dataIndex: 'productBreed', key: 'productBreed', width: 150 },
+  { title: '生产地', scopedSlots: { customRender: 'address' }, key: 'address', width: 150 },
+  { title: '生产企业', scopedSlots: { customRender: 'productionCompany' }, key: 'productionCompany', width: 150 },
+  { title: '生产日期', dataIndex: 'productionDate', key: 'productionDate', width: 120 },
+  { title: '保质期(天)', dataIndex: 'expiryTime', key: 'expiryTime', width: 110 },
+  { title: '木耳图片', scopedSlots: { customRender: 'productPicture' }, key: 'productPicture', align: 'center', width: 100 },
+  { title: '溯源二维码', scopedSlots: { customRender: 'qrcodeId' }, key: 'qrcodeId', align: 'center', width: 120 },
+  { title: '状态', scopedSlots: { customRender: 'status' }, key: 'status', align: 'center', width: 100 },
+  { title: '操作', key: 'operation', scopedSlots: { customRender: 'operation' }, align: 'center', fixed: 'right', width: 280 }
 ]
 const crumbsArr = [
   { name: '当前位置', back: false, path: '' },
