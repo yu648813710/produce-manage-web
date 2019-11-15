eb83d4ec99bc7127786136a5ad871a4792e60831
jiangkun523 <jiangkun13@camelotchina.com>
2019-11-15 16:08:33 +0800

    [姜坤]增加面包屑返回功能

--- src/views/bacteriaBagTaskManagement/config.js
+++ src/views/bacteriaBagTaskManagement/config.js
@@ -42,14 +42,14 @@ const crumbsArr = [
 const addTaskCrumbs = [
   { name: '当前位置', back: false, path: '' },
   { name: '生产管理', back: false, path: '' },
-  { name: '菌包任务管理', back: false, path: '' },
+  { name: '菌包任务管理', back: true, path: '/bacteriabagtaskmanagement' },
   { name: '新增任务', back: false, path: '' }
 ]
 // 详情面包屑
 const dateilCrumbsArr = [
   { name: '当前位置', back: false, path: '' },
   { name: '生产管理', back: false, path: '' },
-  { name: '菌包任务管理', back: false, path: '' },
+  { name: '菌包任务管理', back: true, path: '/bacteriabagtaskmanagement' },
   { name: '任务详情', back: false, path: '' }
 ]
 const stepsArray = [
